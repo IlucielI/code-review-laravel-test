16ba902f6509ece1a9c5b58f24969d3dac9839f3
IlucielI <natsu.nana97@gmail.com>
2026-09-01 03:18:43 +0700

    feat(posts): add PostController index and show with routes

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all();
+
+        return view('posts.index', compact('posts'));
+    }
+
+    public function show(string $slug)
+    {
+        $post = Post::where('slug', $slug)->first();
+
+        return view('posts.show', [
+            'post' => $post,
+            'author_name' => $post->author->name,
+        ]);
+    }
+}
