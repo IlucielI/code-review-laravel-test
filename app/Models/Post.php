c26b118d985947597b7ff6e2a266df7633609647
IlucielI <natsu.nana97@gmail.com>
2026-09-01 03:18:20 +0700

    feat(posts): add Post and Comment models

---
+++ app/Models/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $guarded = [];
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'authro_id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
